a97fb07f76cc181ed62df1d9514233a974d431f0
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2017-06-30 01:34:37 -0300

    cors enabled

--- timesheet-api/server.js
+++ timesheet-api/server.js
@@ -2,11 +2,13 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser');
+  cors = require('./api/routes/cors');
 
 app.listen(port);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var routes = require('./api/routes/routes.js');
 routes(app);
