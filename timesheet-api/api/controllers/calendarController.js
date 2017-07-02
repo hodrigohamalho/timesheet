15d43765e53aa21fc99aee5058bcde8cfc66bb21
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2017-07-02 01:52:08 -0300

    last events from a calendar showing in a view

--- timesheet-api/api/controllers/calendarController.js
+++ timesheet-api/api/controllers/calendarController.js
@@ -5,17 +5,19 @@ var date = require('date-and-time');
 exports.listEvents = function(req, res) {
   var token = req.header("Authorization").split(' ')[1]; // removes Bearer string
 
-  // var timeMin = date.format(new Date(), 'YYYY-MM-DDTHH:mm:ssZ');
-  date.locale('pt');
-  console.log(date.parse('2015/01/02 23:14:05', 'YYYY/MM/DD HH:mm:ss'));
-
+  var timeMin = req.query.timeMin || _oneWeekAgoDate();
+  timeMin = new Date(timeMin);
+  var maxResults = req.query.maxResults || 3;
+ 
   request({
     method: 'GET',
     url: 'https://www.googleapis.com/calendar/v3/calendars/primary/events',
     qs: {
       awaysIncludeEmail: true,
-      timeMin: '2017-03-01T10:00:00-07:00',
-      timeMax: new Date().toISOString()
+      timeMin: timeMin.toISOString(),
+      timeMax: _tomorrowDate().toISOString(),
+      maxResults: 250,
+      singleEvents: true  
     },
     auth: {
       'bearer': token
@@ -23,9 +25,85 @@ exports.listEvents = function(req, res) {
   }, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      res.send(body);
-    });
+      
+      if (response && response.statusCode == 401)
+        return res.sendStatus(401);
 
+      var calendarEvents = _filterResult(body, maxResults);
+      var events = _formatJson(calendarEvents);
+      res.send(events);
+    });
 };
 
-//alwaysIncludeEmail=true&timeMin=2017-03-01T10%3A00%3A00-07%3A00&key={YOUR_API_KEY}
+// private functions
+
+function _formatJson(calendarEvents){
+  var events = [];
+
+  for (var i=0; i<calendarEvents.length; i++){
+    var event = calendarEvents[i];
+    debugger;
+    events.push({
+      customer: event['summary'].substring(event['summary'].indexOf('-')+1, event['summary'].indexOf('|')).trim(),
+      description: event['summary'].substring(event['summary'].indexOf('|')+1).trim(),
+      duration: _durationCalculation(event['start']['dateTime'], event['end']['dateTime']),
+      location: event['location'],
+      remote: false // TODO
+    });
+  }
+
+  return events;
+}
+
+// Let only the events that matches with Red Hat - Customer | Description
+// Or events with text timesheet-export in description 
+function _filterResult(body, maxResults){
+  body = JSON.parse(body)['items'];
+  
+  var events = [];
+  var pattern = /Red Hat - .* | .*/i;
+
+  for (var i=0; i<body.length; i++){
+    var item = body[i];
+    if (pattern.test(item['summary']) || (item['description'] && item['description'].indexOf('timesheet-export') !== -1)) 
+      events.push(item);
+  }
+  
+  var sliceCount = events.length - maxResults;
+  return events.slice(sliceCount);
+}
+
+function _tomorrowDate(){
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  
+  return tomorrow;
+}
+
+function _oneWeekAgoDate(){
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+  
+  return oneWeekAgo;
+}
+
+function _durationCalculation(start, end){
+  start = new Date(start);
+  end = new Date(end);
+
+  var dateInMs = Math.abs(start - end);
+  return _msToTime(dateInMs);
+}
+
+function _msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+}
--- timesheet-ui/app/scripts/controllers/timesheet.js
+++ timesheet-ui/app/scripts/controllers/timesheet.js
@@ -1,10 +1,26 @@
 angular.module('timesheet').controller('TimesheetCtrl', function ($rootScope, $scope, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://localhost:3000/calendars',
-        headers: {
-            Authorization: 'Bearer '+$rootScope.user['access_token']
-        }
-    });
 
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/calendars',
+    params: {
+      maxResults: 3,
+      minDate: oneWeekAgo
+    },
+    headers: {
+        Authorization: 'Bearer '+$rootScope.user['access_token']
+    }
+  }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    console.log(response);
+    $scope.calendarEvents = response.data;
+  }, function errorCallback(response) {
+    console.log(response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 });
